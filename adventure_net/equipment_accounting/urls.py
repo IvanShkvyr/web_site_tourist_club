93ae6fb5f4afb72ac9697346697c13f1a08e403c
IvanShkvyr <Gis2011I@gmail.com>
2023-04-01 09:37:52 +0300

    add equipment html pages

--- adventure_net/equipment_accounting/urls.py
+++ adventure_net/equipment_accounting/urls.py
@@ -5,10 +5,11 @@ app_name = "equipment"
 
 urlpatterns = [
     path("checker/", views.checker, name="checker"),
-    # path("", views.get_equipments, name="get_equipments"), # table of equipment
-    # path("detail/<int:equipment_id>", views.detail_equipment, name="detail_equipment"), # show detail about equipment
-    # path("add_equipment/", views.add_equipment, name="add_equipment"), # add equipment to table
-    # path("change_equipment/<int:equipment_id>", views.change_equipment, name="change_equipment"), # change equipment in table
+    path("", views.get_equipments, name="get_equipments"), # table of equipment
+    path("detail/<int:equipment_id>", views.detail_equipment, name="detail_equipment"), # show detail about equipment
+    path("add_equipment/", views.add_equipment, name="add_equipment"), # add equipment to table
+    path("change_equipment/<int:equipment_id>", views.change_equipment, name="change_equipment"), # change equipment in table
+    path("delete_equipment/<int:equipment_id>", views.delete_equipment, name="delete_equipment"), # delete equipment in table
     path("category/", views.get_category, name="get_category"), # table of equipment categories
     path("category/add_category", views.add_category, name="add_category"), # add equipment category
     path("category/change_category/<int:category_id>", views.change_category, name="change_category"), # change equipment category
--- adventure_net/equipment_accounting/views.py
+++ adventure_net/equipment_accounting/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .forms import EquipmentsCategoriesForm
+from .forms import EquipmentsForm, EquipmentsCategoriesForm
 from .models import Equipments, EquipmentsCategories
 
 
@@ -64,7 +64,56 @@ def delete_category(request, category_id):
     return render(request, "equipment_accounting/delete_category.html")
 
 
+def get_equipments(request):
+    pass
+
+
+def add_equipment(request):
+    categories = EquipmentsCategories.objects.all()
+    if request.method == "POST":
+        form = EquipmentsForm(request.POST)
+        print("3-----------------")
+        print(form)
+        print("4-----------------")
+        if form.is_valid():
+
+            print(form)
+            print("5-----------------")
+            data_equipment = form.save(commit=False)
+            data_equipment.save()
+            # choice_categories = EquipmentsCategories.objects.filter(equipment_category_name__in=request.POST.getlist('categories'))
+            # for category in choice_categories:
+            #     data_equipment.equipment_category.add(category)
+
+            return redirect(to='equipment:get_equipments')
+        else:
+            return render(
+                            request,
+                            "equipment_accounting/add_equipment.html",
+                            context={"form": form, "categories": categories}
+                        )
+    return render(
+                    request,
+                    "equipment_accounting/add_equipment.html",
+                    context={"form": EquipmentsForm(), "categories": categories}
+                 )
+
+    #         data_equipment_categorie = form.save(commit=False)
+    #         data_equipment_categorie.save()
+    #         return redirect(to='equipment:get_category')
+    #     else:
 
 
 
+
+def detail_equipment(request, category_id):
+    pass
+
+
+def change_equipment(request, category_id):
+    pass
+
+
+def delete_equipment(request, category_id):
+    pass
 
